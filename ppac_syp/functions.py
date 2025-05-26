afc171b36ada1491b6f1d73f3a7c8c7141e4154c
Peter Carlton <pmcarlton@mac.com>
2025-05-26 12:58:25 +0900

    added more functions for embedding segments etc.

--- ppac_syp/functions.py
+++ ppac_syp/functions.py
@@ -5,6 +5,106 @@
 import h5py
 import numpy as np
 import pandas as pd
+import plotly.express as px
+import ruptures as rpt
+import umap
+
+
+def get_protein_segments(emb_dict, max_bkps_per100aa=3):
+    ## from A. Moses bioRχiv paper 2025-03
+    """
+    Define the boundaries of protein segments using change point analysis.
+
+    Parameters:
+    emb_dict: a dictionary of UniProt protein IDs as keys and ProtT5 per-residue embeddings as values
+    max_bkps_per100aa: an integer that represents the maximum number of boundaries per 100 amino acids in the protein
+
+    Returns:
+    protein_segments: a dictionary of Uniprot protein IDs as keys and a list of boundaries as values
+    """
+
+    protein_segments = {}
+
+    # for each protein and embedding
+    for protein, emb in emb_dict.items():
+        n_boundaries = max(int(emb.shape[0] * max_bkps_per100aa / 100), 1)
+
+        # try to get the breakpoints from change point analysis
+        try:
+            alg = rpt.Window(width=30, model="rbf", jump=1).fit(emb)
+            # n_bkps is the maximum number of breakpoints allowed in this protein
+            boundaries = alg.predict(n_bkps=n_boundaries)
+            # convert boundaries to segments
+            my_segments = [[0, boundaries[0]]]
+            my_segments.extend(
+                [[boundaries[ii], boundaries[ii + 1]] for ii in range(len(boundaries) - 1)]
+            )
+        # this algorithm can fail if the maximum number of breakpoints is unreasonable
+        except:
+            print("Failed", protein, "number of boundaries", n_boundaries)
+            my_segments = "Failed"
+            raise
+
+        protein_segments[protein] = my_segments
+
+    return protein_segments
+
+
+def get_protein_segment_embeddings(emb_dict, protein_segment_boundaries):
+    ## from A. Moses bioRχiv paper 2025-03
+    """
+    Calculates the segment embedding for each segment defined by the change point analysis
+
+    Parameters:
+    emb_dict: a dictionary of UniProt protein IDs as keys and ProtT5 per-residue embeddings as values
+    protein_segment_boundaries: a dictionary of Uniprot protein IDs as keys and a list of boundaries as values
+
+    Returns:
+    protein_segment_embeddings: a dictionary of "ID start-stop" as keys and segment embeddings as values
+        IDs are UniProt protein IDs, start and stop corresponds to the positions of the segment in the
+        original protein sequence, and segment embeddings are vectors of size 1x1024
+    """
+
+    protein_segment_embeddings = {}
+
+    # for each protein with an embedding
+    for protein, emb in emb_dict.items():
+        # if the segmentation did not fail
+        if protein_segment_boundaries[protein] != "Failed":
+            # generate a "segment embedding" for each segment of the protein
+            for segment in protein_segment_boundaries[protein]:
+                seg_emb = np.mean(emb[segment[0] : segment[1], :], axis=0)
+                key = protein + " " + str(segment[0]) + "-" + str(segment[1])
+
+                protein_segment_embeddings[key] = seg_emb
+
+    return protein_segment_embeddings
+
+
+def plot_embed_segments(filepath):
+    # filepath="iho1_minizoo_per_residue_embeddings.h5"
+    with h5py.File(filepath, "r") as iho:
+        els = {}
+        for x in iho:
+            els[x] = np.array(iho[x])
+
+    ihosegs = get_protein_segments(els)
+    ihosegembs = get_protein_segment_embeddings(els, ihosegs)
+
+    pnl = []
+    v = np.zeros((78, 1024))
+    o = 0
+    for pn, em in ihosegembs.items():
+        pnl.append(pn)
+        v[o] = em
+        o = o + 1
+
+    reducer = umap.UMAP(metric="cosine")
+    vu = reducer.fit_transform(v)
+    pnlab = []
+    for i in pnl:
+        pnlab.append(np.mean([int(x) for x in i.split(" ")[-1].split("-")]))
+    px.scatter(x=vu[:, 0], y=vu[:, 1], hover_name=pnl, color=pnlab)
 
 
 def cdist(x, y):
@@ -30,6 +130,21 @@ def get_emb_by_match(uid, filepath):
         kl = list(fh.keys())
         zk = [x for x in kl if uid in x]
         print(zk)
+        emb = {}
+        for i in zk:
+            emb[i] = np.array(fh[i].astype("float32"))
+    return emb
+
+
+def get_embs_by_match(uids, filepath):
+    # supply uids as list
+    with h5py.File(filepath) as fh:
+        kl = list(fh.keys())
+        zk = []
+        for theuid in uids:
+            zk.append([x for x in kl if theuid in x])
+        if len(zk) > 1:
+            zk = [x for item in zk for x in item]  # flatten if needed
         emb = {}
         for i in zk:
             emb[i] = np.array(fh[i].astype("float32"))
